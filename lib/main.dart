01779af22f87a20cab0b9fd04ac011adb2f93e7a
Vitafresh <Vitas25v>
2021-03-16 00:42:51 +0200

    Dependancies

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //generateWordPairs().forEach(print);
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            child: Text('Hello!'),
+          ),
         ),
       ),
     );
