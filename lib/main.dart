b79e5ee362aa92b82ec5896e8f7c02fc9da6a06b
Vitafresh <Vitas25v>
2021-03-18 20:19:11 +0200

    Before pull

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  AudioPlayer audioPlayer = AudioPlayer();
+
+  void playAudio(){
+    print('Play audio!');
+    //audioPlayer.play('assets/note1.wav',isLocal: true);
+    //play();
+    }
+
+    void play() async {
+      int result = await audioPlayer.play('assets/note1.wav',isLocal: true);
+      if (result == 1) {
+        // success
+      }
+    }
+
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     //generateWordPairs().forEach(print);
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(
-            child: Text('Hello!'),
-          ),
-        ),
+            child: Column(
+          children: [
+            TextButton(
+                onPressed: () {
+                  print('Button pressed');
+                },
+                child: Text('Button')),
+            TextButton(
+                onPressed: () {
+                  playAudio();
+                },
+                child: Text('Button2')),
+          ],
+        )),
       ),
     );
   }
